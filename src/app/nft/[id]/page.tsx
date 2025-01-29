d2bd5bb1a5a15223ce87d681c9c39c7d3b788268
David <goyaldavid55@gmail.com>
2025-01-29 23:05:35 +0530

    fixed bug

--- src/app/nft/[id]/page.tsx
+++ src/app/nft/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ThreeScene from "./ar";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
 	const id = (await params).id;
 	return <ThreeScene index={Number(id)} />;
 }
