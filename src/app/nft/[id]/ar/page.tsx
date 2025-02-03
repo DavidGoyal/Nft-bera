11ed68c6a6ca4461c84a2c5972a6958b68842ff7
David <goyaldavid55@gmail.com>
2025-02-03 10:50:05 +0530

    maybe added ar

--- src/app/nft/[id]/ar/page.tsx
+++ src/app/nft/[id]/ar/page.tsx
@@ -1,30 +1,27 @@
 "use client";
 
 import { ModelViewerElement } from "@google/model-viewer";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 export default function Home() {
 	const model = new ModelViewerElement();
 	model.src =
 		"https://kingdomly-creator-bucket.s3.us-east-2.amazonaws.com/cubhub-glbs/glb-updated/glb/105.glb";
-	return (
-		<div>
-			<model-viewer
-				src={model.src}
-				alt="A 3D model of a cube"
-				ar
-				ar-modes="webxr scene-viewer quick-look"
-				camera-controls
-				environment-image="neutral"
-				interaction-policy="allow-when-focused"
-				interaction-prompt="when-focused"
-				interaction-prompt-threshold="medium"
-				loading="eager"
-				shadow-intensity="1"
-				shadow-softness="1"
-				camera-target="[0, 0, 0]"
-				camera-fov="50"
-			></model-viewer>
-		</div>
-	);
+
+	return React.createElement("model-viewer", {
+		src: model.src,
+		alt: "A 3D model of a cube",
+		ar: true,
+		"ar-modes": "webxr scene-viewer quick-look",
+		"camera-controls": true,
+		"environment-image": "neutral",
+		"interaction-policy": "allow-when-focused",
+		"interaction-prompt": "when-focused",
+		"interaction-prompt-threshold": "medium",
+		loading: "eager",
+		"shadow-intensity": "1",
+		"shadow-softness": "1",
+		"camera-target": "[0, 0, 0]",
+		"camera-fov": "50",
+	});
 }
