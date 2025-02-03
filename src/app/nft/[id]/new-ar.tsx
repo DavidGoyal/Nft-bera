3d2c49e8ad1c917fa675f0fbdbf550802d8f8cbb
David <goyaldavid55@gmail.com>
2025-02-03 11:52:02 +0530

    maybe added ar

--- src/app/nft/[id]/new-ar.tsx
+++ src/app/nft/[id]/new-ar.tsx
@@ -64,7 +64,7 @@ const ThreeScene = ({ index }: { index: number }) => {
 
 	return (
 		<div
-			className="min-h-screen w-screen bg-gradient-to-b from-black via-[#010044] to-black relative"
+			className="h-screen w-screen bg-gradient-to-b from-black via-[#010044] to-black relative"
 			ref={containerRef}
 		>
 			<Header />
